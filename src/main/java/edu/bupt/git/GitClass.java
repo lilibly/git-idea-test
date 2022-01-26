1b1107ce4e31c28e1f1d437be27e9766defbaf3d
lili <3025717373@qq.com>
2022-01-26 22:26:30 +0800

    first commit

---
+++ src/main/java/edu/bupt/git/GitClass.java
@@ -0,0 +1,13 @@
+package edu.bupt.git;
+
+/**
+ * @author: lili
+ * @date: 2022/1/26 22:13
+ * @version: 1.0
+ * @description:  测试git在idea的使用
+ */
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
